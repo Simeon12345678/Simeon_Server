343551a6b8e6e881fb6ec73bea55aa6b32dc660b
Simeon12345678 <simeon.milic@elev.ga.ntig.se>
2025-05-07 11:09:25 +0200

    working

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,13 @@ public class Main {
         ServerSocket server = null;
         Socket client;
 
+        // default port
         int port = 3000;
-
         if (args.length >= 1) {
             port = Integer.parseInt(args[0]);
         }
 
+        // create the socket
         try {
             server = new ServerSocket(port);
         } catch (IOException ie) {
@@ -20,32 +21,40 @@ public class Main {
             System.exit(1);
         }
 
+        // main loop waits for client data and replies
         while (true) {
             try {
+                // wait for connection
                 System.out.println("Waiting for connection");
                 client = server.accept();
-                System.out.println("Connection succesful");
+
+                System.out.println("Connection successful");
                 String clientHost = client.getInetAddress().getHostAddress();
                 int clientPort = client.getPort();
-                System.out.println("Client host= " + clientHost + " Client port = " + clientPort);
+                System.out.println("Client host = " + clientHost + " Client port = " + clientPort);
 
+                // read data
                 InputStream clientIn = client.getInputStream();
                 BufferedReader br = new BufferedReader(new InputStreamReader(clientIn));
 
-                System.out.println("Write a SIMPLE math problem ie addition, multiplication, subtraction or division in the format of example 1+2");
                 String msgFromClient = br.readLine();
-                int index = 1;
+                System.out.println("Message received " + msgFromClient);
                 // format of 1+2 as a string means the operator is at index 1 which is what we check for
+                int index = 1;
 
-                if (msgFromClient != null && msgFromClient.equalsIgnoreCase("bye")) {
+
+                // send response
+                if (msgFromClient != null && !msgFromClient.equalsIgnoreCase("bye")) {
+                    // convert the given message into operators and numbers to do math with
                     char operator = msgFromClient.charAt(index);
-                    int num1 = msgFromClient.charAt(0);
-                    int num2 = msgFromClient.charAt(2);
+                    int num1 = Character.getNumericValue(msgFromClient.charAt(0));
+                    int num2 = Character.getNumericValue(msgFromClient.charAt(2));
                     int ans;
 
                     OutputStream clientOut = client.getOutputStream();
                     PrintWriter pw = new PrintWriter(clientOut, true);
 
+                    // select the operators and printout the answers
                     switch (operator) {
                         case '+':
                             ans = num1 + num2;
@@ -64,8 +73,14 @@ public class Main {
                             pw.println(msgFromClient + " is " + ans);
                     }
                 }
+
+                if (msgFromClient != null && msgFromClient.equalsIgnoreCase("bye")) {
+                    server.close();
+                    client.close();
+                    break;
+                }
             } catch (IOException ie) {
-                System.out.println("bad");
+                System.out.println("Error in communicating with the client " + ie);
             }
         }
     }
